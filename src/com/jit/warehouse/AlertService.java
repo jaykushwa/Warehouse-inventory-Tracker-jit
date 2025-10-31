502748a089cb463d8d351a0b34d739b8c2346f71
jay kushwah <jpkushwa123@gmail.com>
2025-10-31 12:33:20 +0530

    Delete src directory

--- src/com/jit/warehouse/AlertService.java
+++
@@ -1,9 +0,0 @@
-package com.jit.warehouse;
-
-public class AlertService implements StockObserver {
-    @Override
-    public void onLowStock(Product product) {
-        System.out.println("[ALERT] Restock needed for " + product.getName()
-                + " (ID: " + product.getProductId() + ") - only " + product.getQuantity() + " left.");
-    }
-}
